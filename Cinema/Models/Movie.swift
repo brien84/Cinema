a6144da3a4f16bcd9f117ec0adba68e7b36be49f
brien84 <houseinblues@gmail.com>
2019-09-21 21:44:44 +0300

    Add and sort initial files

---
+++ Cinema/Models/Movie.swift
@@ -0,0 +1,9 @@
+//
+//  Movie.swift
+//  Cinema
+//
+//  Created by Marius on 21/09/2019.
+//  Copyright © 2019 Marius. All rights reserved.
+//
+
+import Foundation
---
+++ Cinema/Models/Showing.swift
@@ -0,0 +1,9 @@
+//
+//  Showing.swift
+//  Cinema
+//
+//  Created by Marius on 21/09/2019.
+//  Copyright © 2019 Marius. All rights reserved.
+//
+
+import Foundation
