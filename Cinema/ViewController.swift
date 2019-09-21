571de39d109cff1f4f3f5b9bccccd750cc92ef8d
brien84 <houseinblues@gmail.com>
2019-09-21 20:43:30 +0300

    Initial commit

---
+++ Cinema/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cinema
+//
+//  Created by Marius on 21/09/2019.
+//  Copyright © 2019 Marius. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
