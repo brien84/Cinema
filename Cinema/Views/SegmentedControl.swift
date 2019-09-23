71f5356f48f9da89642dbd2341ab7e5bbda0ca40
brien84 <houseinblues@gmail.com>
2019-09-23 21:56:27 +0300

    Add SegmentedControl class

---
+++ Cinema/Views/SegmentedControl.swift
@@ -0,0 +1,25 @@
+//
+//  SegmentedControl.swift
+//  Cinema
+//
+//  Created by Marius on 23/09/2019.
+//  Copyright © 2019 Marius. All rights reserved.
+//
+
+import UIKit
+
+enum DailyViewControllerSegments: Int, CaseIterable {
+    case Filmai
+    case Seansai
+}
+
+class SegmentedControl: UISegmentedControl {
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        self.removeAllSegments()
+        DailyViewControllerSegments.allCases.forEach { segment in
+            self.insertSegment(withTitle: "\(segment)", at: segment.rawValue, animated: false)
+        }
+    }
+}
