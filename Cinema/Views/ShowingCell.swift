294e50eaf6ffeb2aae3e83f930a32a76b898bc6b
brien84 <houseinblues@gmail.com>
2019-09-23 18:50:41 +0300

    Add ShowingCell

---
+++ Cinema/Views/ShowingCell.swift
@@ -0,0 +1,19 @@
+//
+//  ShowingCell.swift
+//  Cinema
+//
+//  Created by Marius on 23/09/2019.
+//  Copyright © 2019 Marius. All rights reserved.
+//
+
+import UIKit
+
+class ShowingCell: UITableViewCell {
+    
+    @IBOutlet weak var title: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
