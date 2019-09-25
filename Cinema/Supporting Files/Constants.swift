a7277b8481709a63dcee62340d902bd8f80066b7
brien84 <houseinblues@gmail.com>
2019-09-25 19:03:59 +0300

    Move DailyViewControllerSegments to new Constants file

---
+++ Cinema/Supporting Files/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Cinema
+//
+//  Created by Marius on 25/09/2019.
+//  Copyright © 2019 Marius. All rights reserved.
+//
+
+import Foundation
+
+enum DailyViewControllerSegments: Int, CaseIterable {
+    case Filmai
+    case Seansai
+}
