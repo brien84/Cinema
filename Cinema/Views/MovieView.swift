19d73a9bd0941f2185ebc287ac8c57137d1eba69
brien84 <houseinblues@gmail.com>
2019-09-28 19:10:02 +0300

    Add MovieView class

---
+++ Cinema/Views/MovieView.swift
@@ -0,0 +1,20 @@
+//
+//  MovieView.swift
+//  Cinema
+//
+//  Created by Marius on 28/09/2019.
+//  Copyright © 2019 Marius. All rights reserved.
+//
+
+import UIKit
+
+class MovieView: UIView {
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var originalTitle: UILabel!
+    @IBOutlet weak var runtime: UILabel!
+    @IBOutlet weak var rated: UILabel!
+    @IBOutlet weak var genre: UILabel!
+    @IBOutlet weak var country: UILabel!
+    @IBOutlet weak var released: UILabel!
+    @IBOutlet weak var plot: UILabel!
+}
