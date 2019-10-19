ba006b007a2f62617ecb6eb995541f4edf248915
brien84 <houseinblues@gmail.com>
2019-10-19 19:38:12 +0300

    Update Color constants

--- Cinema/Supporting Files/Constants.swift
+++ Cinema/Supporting Files/Constants.swift
@@ -11,10 +11,9 @@ import UIKit
 struct Constants {
     struct Colors {
         static let blue = UIColor(red: 30/255, green: 144/255, blue: 255/255, alpha: 1.0)           //#1e90ff
-        static let lightBlue = UIColor(red: 112/255, green: 161/255, blue: 255/255, alpha: 1.0)     //#70a1ff
+        static let lightBlue = UIColor(red: 30/255, green: 144/255, blue: 255/255, alpha: 0.50)     //#70a1ff
         static let light = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1.0)         //#ffffff
-        static let lightGray = UIColor(red: 241/255, green: 242/255, blue: 246/255, alpha: 1.0)     //#f1f2f6
-        static let darkGray = UIColor(red: 87/255, green: 86/255, blue: 111/255, alpha: 1.0)        //#57606f
+        static let gray = UIColor(red: 87/255, green: 86/255, blue: 111/255, alpha: 1.0)            //#57606f
         static let dark = UIColor(red: 47/255, green: 53/255, blue: 66/255, alpha: 1.0)             //#2f3542
     }
     
