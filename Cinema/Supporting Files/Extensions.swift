42502b608d8ffbd513e777f50f277530695716aa
brien84 <houseinblues@gmail.com>
2019-10-22 23:23:20 +0300

    Add uniqued() Array extension

--- Cinema/Supporting Files/Extensions.swift
+++ Cinema/Supporting Files/Extensions.swift
@@ -8,6 +8,13 @@
 
 import Foundation
 
+extension Array where Element: Hashable {
+    func uniqued() -> [Element] {
+        var seen = Set<Element>()
+        return filter{ seen.insert($0).inserted }
+    }
+}
+
 extension Date {
     enum asStringFormat {
         case fullDate
