e77c41aeae8e4d36a2799f0fd69b3e8e15103799
brien84 <houseinblues@gmail.com>
2020-01-11 23:52:08 +0200

    Add MovieDetailView

---
+++ Cinema/Views/MovieDetailView.swift
@@ -0,0 +1,22 @@
+//
+//  MovieDetailView.swift
+//  Cinema
+//
+//  Created by Marius on 2019-12-27.
+//  Copyright © 2019 Marius. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailView: UIView {
+
+    @IBOutlet weak var poster: NetworkImageView!
+    @IBOutlet weak var ageRating: UILabel!
+    @IBOutlet weak var genre: UILabel!
+    @IBOutlet weak var releaseDate: UILabel!
+    @IBOutlet weak var duration: UILabel!
+    @IBOutlet weak var plot: UILabel!
+    
+}
+
+
