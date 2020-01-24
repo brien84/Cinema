e442a456e8abc765b27ab1b55cae1745cde1744b
brien84 <houseinblues@gmail.com>
2020-01-24 20:41:32 +0200

    Add `is3D` property to `Showing` model

--- Cinema/Models/Showing.swift
+++ Cinema/Models/Showing.swift
@@ -12,12 +12,14 @@ final class Showing: Decodable {
     let city: String
     let date: Date
     let venue: String
+    let is3D: Bool
     var parentMovie: Movie?
     
-    init(city: String, date: Date, venue: String, parentMovie: Movie) {
+    init(city: String, date: Date, venue: String, is3D: Bool, parentMovie: Movie) {
         self.city = city
         self.date = date
         self.venue = venue
+        self.is3D = is3D
         self.parentMovie = parentMovie
     }
 }
