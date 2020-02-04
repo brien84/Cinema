6010dcc66b86439b10c46db56b8b4e899367e5ce
brien84 <houseinblues@gmail.com>
2020-02-04 20:04:40 +0200

    Refactor `Constants` to utilise type extensions instead of structs

--- Cinema/Supporting Files/Constants.swift
+++ Cinema/Supporting Files/Constants.swift
@@ -8,46 +8,26 @@
 
 import UIKit
 
-struct Colors {
-    static let dark = UIColor(named: "dark")!
-    static let gray = UIColor(named: "gray")!
-    static let light = UIColor(named: "light")!
-    static let red = UIColor(named: "red")!
-    static let transparentBlack = UIColor(named: "transparentBlack")!
+extension CGFloat {
+    static let screenWidth = UIScreen.main.bounds.width
+    static let screenHeight = UIScreen.main.bounds.height
 }
 
-struct Constants {
-    struct Fonts {
-        struct DateContainerCell {
-            static let title = UIFont(name: "HelveticaNeue-Medium", size: 22.0)!
-            static let originalTitle = UIFont(name: "HelveticaNeue-ThinItalic", size: 16.0)!
-            static let label = UIFont(name: "HelveticaNeue-Light", size: 18.0)!
-        }
-        
-        struct MovieShowingCell {
-            static let time = UIFont(name: "HelveticaNeue-Medium", size: 36.0)!
-            static let date = UIFont(name: "HelveticaNeue-Light", size: 20.0)!
-            static let venue = UIFont(name: "HelveticaNeue-Light", size: 18.0)!
-        }
-        
-        struct MovieView {
-            static let staticLabel = UIFont(name: "HelveticaNeue-Thin", size: 14.0)!
-            static let dynamicLabel = UIFont(name: "HelveticaNeue-Light", size: 17.0)!
-            static let plot = UIFont(name: "HelveticaNeue-Light", size: 16.0)!
-        }
-        
-        static let errorLabel = UIFont(name: "HelveticaNeue-Light", size: 18.0)!
-        static let optionsCell = UIFont(name: "HelveticaNeue-Light", size: 20.0)!
-    }
-    
-    struct Images {
-        static let options = UIImage(named: "options")!
-        static let left = UIImage(named: "arrowLeft")!
-        static let right = UIImage(named: "arrowRight")!
-    }
-    
-    struct URLs {
-        static let api = URL(string: "https://movies.ioys.lt/all")!
-        //static let api = URL(string: "http://localhost:8080/all")!
-    }
+extension UIColor {
+    static let darkC = UIColor(named: "dark")!
+    static let grayC = UIColor(named: "gray")!
+    static let lightC = UIColor(named: "light")!
+    static let redC = UIColor(named: "red")!
+    static let transparentBlackC = UIColor(named: "transparentBlack")!
+}
+
+extension UIImage {
+    static let options = UIImage(named: "options")!
+    static let left = UIImage(named: "arrowLeft")!
+    static let right = UIImage(named: "arrowRight")!
+}
+
+extension URL {
+    static let api = URL(string: "https://movies.ioys.lt/all")!
+    //static let api = URL(string: "http://localhost:8080/all")!
 }
