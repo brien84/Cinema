9ebd19a457e891afc39ce53459a2f74979fca274
brien84 <houseinblues@gmail.com>
2020-02-10 18:52:18 +0200

    Create `DateSelectable` protocol

---
+++ Cinema/Protocols/DateSelectable.swift
@@ -0,0 +1,18 @@
+//
+//  DateSelectable.swift
+//  Cinema
+//
+//  Created by Marius on 2020-02-10.
+//  Copyright © 2020 Marius. All rights reserved.
+//
+
+import Foundation
+
+protocol DateSelectable {
+    var selectedDate: Date { get }
+    var isFirstDateSelected: Bool { get }
+    var isLastDateSelected: Bool { get }
+    
+    func previousDate()
+    func nextDate()
+}
