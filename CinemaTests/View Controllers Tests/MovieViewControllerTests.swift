eeb67a248932c16826dd7222077cbcf9a4b5e7ba
brien84 <houseinblues@gmail.com>
2020-02-16 22:08:37 +0200

    Refactor `MovieViewControllerTests`

---
+++ CinemaTests/View Controllers Tests/MovieViewControllerTests.swift
@@ -0,0 +1,53 @@
+//
+//  MovieContainerVCTests.swift
+//  CinemaTests
+//
+//  Created by Marius on 15/11/2019.
+//  Copyright © 2019 Marius. All rights reserved.
+//
+
+import XCTest
+@testable import Cinema
+
+class MovieViewControllerTests: XCTestCase {
+
+    var sut: MovieViewController!
+
+    override func setUp() {
+        sut = MovieViewController(with: movie)
+    }
+
+    override func tearDown() {
+        sut = nil
+    }
+
+    func testViewBackgroundColorIsTransparentBlack() {
+        // when
+        _ = sut.view
+
+        // then
+        XCTAssertEqual(sut.view.backgroundColor, .transparentBlackC)
+    }
+
+    func testSegmentedControlSomeSegmentIsSelected() {
+        // when
+        _ = sut.view
+
+        // then
+        XCTAssertNotEqual(sut.segmentedControl.selectedSegmentIndex, -1)
+    }
+
+    func testNavigationItemTitleIsMovieTitle() {
+        // when
+        _ = sut.view
+
+        // then
+        XCTAssertEqual(sut.navigationItem.title, movie.title)
+    }
+
+    // MARK: - Test Helpers
+
+    private var movie: Movie {
+        return TestHelper.getMovie()
+    }
+}
