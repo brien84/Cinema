4aa068d2f491e549dc4b0dcdde58139c37ee99bf
brien84 <houseinblues@gmail.com>
2020-07-21 21:16:02 +0300

    Implement `MovieDetailsView` initialisation

--- Cinema/View Controllers/MovieDetailsViewController.swift
+++ Cinema/View Controllers/MovieDetailsViewController.swift
@@ -10,10 +10,17 @@ import UIKit
 
 final class MovieDetailsViewController: UIViewController {
 
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: "MovieDetailsView", bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
 }
