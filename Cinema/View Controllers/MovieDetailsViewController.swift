9b3ed239ba04506722f35e47267b476dfeca6047
brien84 <houseinblues@gmail.com>
2020-07-21 21:10:34 +0300

    Create `MovieDetailsViewController`

---
+++ Cinema/View Controllers/MovieDetailsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MovieDetailsViewController.swift
+//  Cinema
+//
+//  Created by Marius on 2020-07-21.
+//  Copyright © 2020 Marius. All rights reserved.
+//
+
+import UIKit
+
+final class MovieDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
