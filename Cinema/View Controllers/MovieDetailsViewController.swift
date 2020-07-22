fbdcc0a57c86fef639ba4a21b06774cda2aa8804
brien84 <houseinblues@gmail.com>
2020-07-22 23:36:43 +0300

    Add `handleScrollDown(_ offset: CGFloat)` method

--- Cinema/View Controllers/MovieDetailsViewController.swift
+++ Cinema/View Controllers/MovieDetailsViewController.swift
@@ -12,7 +12,13 @@ final class MovieDetailsViewController: UIViewController {
     @IBOutlet private weak var scrollView: UIScrollView!
 
     @IBOutlet private weak var posterView: UIImageView!
+    @IBOutlet private weak var detailsContainer: UIView!
     @IBOutlet private weak var plotLabel: UILabel!
+
+    @IBOutlet private weak var posterHeight: NSLayoutConstraint!
+    @IBOutlet private weak var posterTopToSuperview: NSLayoutConstraint!
+    @IBOutlet private weak var posterBottomToDetailsTop: NSLayoutConstraint!
+    @IBOutlet private weak var detailsBottomToSuperview: NSLayoutConstraint!
 
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
         super.init(nibName: "MovieDetailsView", bundle: nil)
@@ -33,6 +39,41 @@ extension MovieDetailsViewController: UIScrollViewDelegate {
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         let offset = scrollView.contentOffset.y
 
-        print(offset)
+        handleScrollDown(offset)
+    }
+
+    private func handleScrollDown(_ offset: CGFloat) {
+        // Convert offset to positive number for clearer calculations.
+        let offset = -offset
+
+        // If scrolling upwards.
+        if offset <= 0 {
+            posterHeight.constant = 0
+            posterTopToSuperview.constant = 0
+            posterBottomToDetailsTop.constant = 0
+            detailsBottomToSuperview.constant = 0
+            return
+        }
+
+        let multi = 1 + posterBottomToDetailsTop.multiplier
+
+        // `overlap` is distance by which `detailsContainer` overlaps `posterView`.
+        // In other words - how much can we scroll before `detailsContainer` is not overlapping `posterView`.
+        let currentOverlap = (posterView.frame.maxY - detailsContainer.frame.minY) / multi
+        let overlap = currentOverlap + posterBottomToDetailsTop.constant / multi
+
+        if offset > 0, offset <= overlap {
+            posterHeight.constant = 0
+            posterTopToSuperview.constant = offset
+            posterBottomToDetailsTop.constant = offset * multi
+            detailsBottomToSuperview.constant = -offset
+        }
+
+        if offset > overlap {
+            posterHeight.constant = offset - overlap
+            posterTopToSuperview.constant = offset
+            posterBottomToDetailsTop.constant = overlap * multi
+            detailsBottomToSuperview.constant = -overlap
+        }
     }
 }
