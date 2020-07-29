549e9bd35ab678d3b636d203d09941e744f0fc2c
brien84 <houseinblues@gmail.com>
2020-07-29 22:00:38 +0300

    Setup `tableView`

--- Cinema/View Controllers/DateViewController.swift
+++ Cinema/View Controllers/DateViewController.swift
@@ -9,10 +9,12 @@
 import UIKit
 
 final class DateViewController: UIViewController {
+    @IBOutlet private weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
     }
 
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
@@ -22,5 +24,20 @@ final class DateViewController: UIViewController {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+}
 
+extension DateViewController: UITableViewDelegate {
+
+}
+
+extension DateViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        return cell
+    }
 }
