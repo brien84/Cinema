89534a158d01ff0746cb939d18652edcb31a2ed2
brien84 <houseinblues@gmail.com>
2020-07-29 21:50:28 +0300

    Create `DateViewController` class

---
+++ Cinema/View Controllers/DateViewController.swift
@@ -0,0 +1,26 @@
+//
+//  DateViewController.swift
+//  Cinema
+//
+//  Created by Marius on 2020-07-29.
+//  Copyright © 2020 Marius. All rights reserved.
+//
+
+import UIKit
+
+final class DateViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: "DateView", bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
