bff3c84a61fc6c4ac89708557d6aae71097d2d78
brien84 <houseinblues@gmail.com>
2020-08-01 23:48:39 +0300

    Conform to `UICollectionViewDelegateFlowLayout` protocol and layout collection view

--- Cinema/View Controllers/MoviesViewController.swift
+++ Cinema/View Controllers/MoviesViewController.swift
@@ -28,3 +28,24 @@ final class MoviesViewController: UICollectionViewController {
         return cell
     }
 }
+
+extension MoviesViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        inset
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        CGSize(width: cellWidth, height: cellHeight)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        UIEdgeInsets(top: 0.0, left: inset, bottom: 0.0, right: inset)
+    }
+}
+
+extension MoviesViewController {
+    private var inset: CGFloat { collectionView.frame.height * 0.05 }
+
+    private var cellHeight: CGFloat { collectionView.frame.height }
+    private var cellWidth: CGFloat { collectionView.frame.width / 2 - 1.5 * inset }
+}
