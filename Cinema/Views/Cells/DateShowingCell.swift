25b8535e7b8269383115d71fd25d3c7ceeb302f4
brien84 <houseinblues@gmail.com>
2020-08-06 22:29:58 +0300

    Create `DateShowingCell` class

---
+++ Cinema/Views/Cells/DateShowingCell.swift
@@ -0,0 +1,13 @@
+//
+//  DateShowingCell.swift
+//  Cinema
+//
+//  Created by Marius on 2020-08-06.
+//  Copyright © 2020 Marius. All rights reserved.
+//
+
+import UIKit
+
+final class DateShowingCell: UITableViewCell {
+
+}
