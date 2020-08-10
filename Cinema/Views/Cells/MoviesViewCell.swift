aafb2e84d30b2603b637b657a752dba7e31b49dd
brien84 <houseinblues@gmail.com>
2020-08-10 23:41:38 +0300

    Create `MoviesViewCell`

---
+++ Cinema/Views/Cells/MoviesViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MoviesViewCell.swift
+//  Cinema
+//
+//  Created by Marius on 2020-08-10.
+//  Copyright © 2020 Marius. All rights reserved.
+//
+
+import UIKit
+
+final class MoviesViewCell: UICollectionViewCell {
+    
+}
