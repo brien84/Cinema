60cc3a3812256eb3ee3c3b5fed7461f80094c484
brien84 <houseinblues@gmail.com>
2020-08-16 22:26:43 +0300

    Implement `systemLayoutSizeFitting` methods

--- Cinema/Views/Cells/MoviesViewCell.swift
+++ Cinema/Views/Cells/MoviesViewCell.swift
@@ -9,5 +9,36 @@
 import UIKit
 
 final class MoviesViewCell: UICollectionViewCell {
-    
+    @IBOutlet weak var poster: UIImageView!
+    @IBOutlet weak var title: UILabel!
+
+    // iOS 13+
+    override func systemLayoutSizeFitting(
+        _ targetSize: CGSize,
+        withHorizontalFittingPriority horizontalFittingPriority: UILayoutPriority,
+        verticalFittingPriority: UILayoutPriority) -> CGSize {
+
+        calculateCellSize(with: targetSize)
+    }
+
+    // iOS 12
+    override func systemLayoutSizeFitting(_ targetSize: CGSize) -> CGSize {
+        calculateCellSize(with: targetSize)
+    }
+}
+
+extension MoviesViewCell {
+    private var cellWidth: CGFloat {
+        poster.frame.height / 1.5
+    }
+
+    private func calculateCellSize(with targetSize: CGSize) -> CGSize {
+        let size = super.systemLayoutSizeFitting(
+            targetSize,
+            withHorizontalFittingPriority: .fittingSizeLevel,
+            verticalFittingPriority: .required
+        )
+
+        return CGSize(width: cellWidth, height: size.height)
+    }
 }
