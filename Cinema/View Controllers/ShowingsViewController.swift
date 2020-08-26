7a8018c7e69f172544912a55b251ae157b727978
brien84 <houseinblues@gmail.com>
2020-08-26 20:15:00 +0300

    Create `ShowingsViewController`

---
+++ Cinema/View Controllers/ShowingsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ShowingsViewController.swift
+//  Cinema
+//
+//  Created by Marius on 2020-08-26.
+//  Copyright © 2020 Marius. All rights reserved.
+//
+
+import UIKit
+
+final class ShowingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+
+}
