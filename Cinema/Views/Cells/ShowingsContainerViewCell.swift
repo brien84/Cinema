ca5de3160b6203052bfa92ab3010dfd19e47b4ad
brien84 <houseinblues@gmail.com>
2020-08-30 21:14:41 +0300

    Create `ShowingsContainerViewCell`

---
+++ Cinema/Views/Cells/ShowingsContainerViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ShowingsContainerViewCell.swift
+//  Cinema
+//
+//  Created by Marius on 2020-08-30.
+//  Copyright © 2020 Marius. All rights reserved.
+//
+
+import UIKit
+
+final class ShowingsContainerViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var time: UILabel!
+
+}
