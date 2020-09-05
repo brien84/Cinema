8c86ffc370566bd609e557de25ff7d0dcdf68a6f
brien84 <houseinblues@gmail.com>
2020-09-05 23:45:42 +0300

    Add `dateViewHeader`, `dateViewTitle`, `dateViewOriginalTitle`, `dateViewDetails` fonts

--- Cinema/Supporting Files/Fonts.swift
+++ Cinema/Supporting Files/Fonts.swift
@@ -9,17 +9,26 @@
 import UIKit
 
 enum Fonts: String {
+    case dateViewHeader, dateViewTitle, dateViewOriginalTitle, dateViewDetails
     case notFound
 
     private var font: UIFont {
         switch self {
+        case .dateViewHeader:
+            return UIFont.init(name: "HelveticaNeue-Bold", size: 20)!
+        case .dateViewTitle:
+            return UIFont.init(name: "HelveticaNeue-Medium", size: 18)!
+        case .dateViewOriginalTitle:
+            return UIFont.init(name: "HelveticaNeue-LightItalic", size: 15)!
+        case .dateViewDetails:
+            return UIFont.init(name: "HelveticaNeue-Thin", size: 15)!
         default:
             return UIFont.init(name: "BodoniOrnamentsITCTT", size: 50)!
         }
     }
 
-    static func getFont(rawValue: String) -> UIFont  {
-        if let fonts = Fonts(rawValue: rawValue) {
+    static func getFont(_ identifier: String) -> UIFont {
+        if let fonts = Fonts(rawValue: identifier) {
             return fonts.font
         }
 
