09b11285c1fb8a6bbc81a334c49cbffd7d603b64
brien84 <houseinblues@gmail.com>
2020-11-15 16:37:44 +0200

    Add `Movie` and `Showing` extensions for creating test objects

---
+++ CinemaTests/Supporting Files/Models+Testable.swift
@@ -0,0 +1,64 @@
+//
+//  Models+Testable.swift
+//  CinemaTests
+//
+//  Created by Marius on 2020-11-12.
+//  Copyright © 2020 Marius. All rights reserved.
+//
+
+// swiftlint:disable force_try
+import XCTest
+@testable import iKinas
+
+// MARK: - Movie
+
+private struct MovieDouble: Codable {
+    let title: String
+    let originalTitle: String
+    let year: String
+    let ageRating: String?
+    let duration: String?
+    let genres: [String]?
+    let plot: String?
+    let poster: URL?
+    var showings: [Showing]
+}
+
+extension Movie {
+    static func create(_ title: String, _ originalTitle: String, _ year: String, _ ageRating: String,
+                       _ duration: String, _ genres: [String], _ plot: String, _ poster: URL, _ showings: [Showing]) -> Movie {
+        let double = MovieDouble(title: title, originalTitle: originalTitle, year: year, ageRating: ageRating,
+                                 duration: duration, genres: genres, plot: plot, poster: poster, showings: showings)
+
+        let encoder = JSONEncoder()
+        encoder.dateEncodingStrategy = .iso8601
+        let data = try! encoder.encode(double)
+
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .iso8601
+        return try! decoder.decode(Movie.self, from: data)
+    }
+}
+
+// MARK: - Showing
+
+private struct ShowingDouble: Codable {
+    let city: City
+    let date: Date
+    let venue: String
+    let is3D: Bool
+}
+
+extension Showing {
+    static func create(_ city: City, _ date: Date, _ venue: String, _ is3D: Bool) -> Showing {
+        let double = ShowingDouble(city: city, date: date, venue: venue, is3D: is3D)
+
+        let encoder = JSONEncoder()
+        encoder.dateEncodingStrategy = .iso8601
+        let data = try! encoder.encode(double)
+
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .iso8601
+        return try! decoder.decode(Showing.self, from: data)
+    }
+}
