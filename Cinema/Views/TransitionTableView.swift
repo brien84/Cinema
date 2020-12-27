e2023881f7063483d6b333a04459f623dc2a23f0
brien84 <houseinblues@gmail.com>
2020-12-27 19:45:33 +0200

    Create `TransitionTableView` class

---
+++ Cinema/Views/TransitionTableView.swift
@@ -0,0 +1,13 @@
+//
+//  TransitionTableView.swift
+//  Cinema
+//
+//  Created by Marius on 2020-12-19.
+//  Copyright © 2020 Marius. All rights reserved.
+//
+
+import UIKit
+
+final class TransitionTableView: UITableView {
+
+}
