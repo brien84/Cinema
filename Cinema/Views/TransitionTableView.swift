5e08b11b7abc09391b5db539d47671d64a00859c
brien84 <houseinblues@gmail.com>
2020-12-27 19:56:55 +0200

    Add `isTableHeaderViewVisible` computed property

--- Cinema/Views/TransitionTableView.swift
+++ Cinema/Views/TransitionTableView.swift
@@ -12,4 +12,11 @@ final class TransitionTableView: UITableView {
     @IBOutlet private weak var headerMoviesLabel: CustomFontLabel!
     @IBOutlet private weak var headerContainerView: UIView!
     @IBOutlet private weak var headerShowingsLabel: CustomFontLabel!
+
+    /// Returns true if`tableHeaderView` is completely under navigation bar.
+    private var isTableHeaderViewVisible: Bool {
+        guard let header = tableHeaderView else { return false }
+        let distance = safeAreaInsets.top.distance(to: header.frame.maxY)
+        return distance >= contentOffset.y
+    }
 }
