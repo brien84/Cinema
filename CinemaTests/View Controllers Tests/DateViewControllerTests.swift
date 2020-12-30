0fb5ebd41d59894bf412ff06a5397ef76001f22b
brien84 <houseinblues@gmail.com>
2020-12-30 19:22:35 +0200

    Create `DateViewControllerTests`

---
+++ CinemaTests/View Controllers Tests/DateViewControllerTests.swift
@@ -0,0 +1,26 @@
+//
+//  DateViewControllerTests.swift
+//  CinemaTests
+//
+//  Created by Marius on 2020-12-30.
+//  Copyright © 2020 Marius. All rights reserved.
+//
+
+import XCTest
+@testable import iKinas
+
+class DateViewControllerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+
+    }
+
+    override func tearDownWithError() throws {
+
+    }
+
+    func testExample() throws {
+
+    }
+
+}
