cc8d5de1a35bbaa860cd6969d77e46bfba650896
brien84 <houseinblues@gmail.com>
2021-01-10 23:33:13 +0200

    Setup `navigationBar` background

--- Cinema/AppDelegate.swift
+++ Cinema/AppDelegate.swift
@@ -15,11 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
+        let navController = window?.rootViewController as? UINavigationController
+        navController?.navigationBar.shadowImage = UIImage()
+        navController?.navigationBar.setBackgroundImage(color: .secondaryBackground)
+
         // Opens `SettingsViewController` if the app is started for the first time or if UI tests commence.
         if !UserDefaults.standard.isCitySet() || CommandLine.arguments.contains("ui-testing") {
             let storyboard = UIStoryboard(name: "Main", bundle: nil)
             let optionsVC = storyboard.instantiateViewController(withIdentifier: "OptionsVC")
-            let navController = window?.rootViewController as? UINavigationController
             navController?.pushViewController(optionsVC, animated: false)
         }
 
