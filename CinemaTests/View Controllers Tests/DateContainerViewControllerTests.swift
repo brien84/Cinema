b9eb25d73440bee4055a0bb90d92279c07aebd8a
brien84 <houseinblues@gmail.com>
2021-01-13 00:41:07 +0200

    Add `DateContainerViewControllerTests`

---
+++ CinemaTests/View Controllers Tests/DateContainerViewControllerTests.swift
@@ -0,0 +1,24 @@
+//
+//  DateContainerViewControllerTests.swift
+//  CinemaTests
+//
+//  Created by Marius on 2021-01-13.
+//  Copyright © 2021 Marius. All rights reserved.
+//
+
+import XCTest
+
+final class DateContainerViewControllerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+
+    }
+
+    override func tearDownWithError() throws {
+
+    }
+
+    func testExample() throws {
+
+    }
+}
