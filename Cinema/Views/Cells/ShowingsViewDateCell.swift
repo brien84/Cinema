0dcaeb0e51acd7b9f1877ef643f41f1feceaa162
brien84 <houseinblues@gmail.com>
2021-01-23 23:37:24 +0200

    Create `ShowingsViewDateCell`

---
+++ Cinema/Views/Cells/ShowingsViewDateCell.swift
@@ -0,0 +1,13 @@
+//
+//  ShowingsViewDateCell.swift
+//  Cinema
+//
+//  Created by Marius on 2021-01-23.
+//  Copyright © 2021 Marius. All rights reserved.
+//
+
+import UIKit
+
+final class ShowingsViewDateCell: UICollectionViewCell {
+    
+}
