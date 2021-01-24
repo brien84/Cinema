9dea683e6d913e142a9c31e5f86d912213fd5235
brien84 <houseinblues@gmail.com>
2021-01-24 20:07:35 +0200

    Create `ShowingsViewTimeCell`

---
+++ Cinema/Views/Cells/ShowingsViewTimeCell.swift
@@ -0,0 +1,13 @@
+//
+//  ShowingsViewTimeCell.swift
+//  Cinema
+//
+//  Created by Marius on 2021-01-24.
+//  Copyright © 2021 Marius. All rights reserved.
+//
+
+import UIKit
+
+final class ShowingsViewTimeCell: UICollectionViewCell {
+    
+}
