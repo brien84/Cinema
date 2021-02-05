bcb6ef706a5f80d2c721b08054ca93273002acf0
brien84 <houseinblues@gmail.com>
2021-02-05 22:40:20 +0200

    Create `WebViewController`

---
+++ Cinema/View Controllers/WebViewController.swift
@@ -0,0 +1,15 @@
+//
+//  WebViewController.swift
+//  Cinema
+//
+//  Created by Marius on 2021-02-04.
+//  Copyright © 2021 Marius. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+final class WebViewController: UIViewController {
+    @IBOutlet private weak var webView: WKWebView!
+
+}
